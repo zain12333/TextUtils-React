1828a121211ad0e78033acc77418c172b7f590ed
zain <muhammadzain6787@gmail.com>
2025-03-26 14:36:11 +0500

    Router used for  one click and move next page

--- src/App.js
+++ src/App.js
@@ -5,87 +5,52 @@ import TextForm from './component/TextForm';
 import { useState } from 'react';
 import Alert from './component/Alert';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+// is used for web page one click and get result move other page 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);  
 
-  const [mode , setMode]= useState('light');
-  // so this is used for if light so navgation is light and dark so dark 
-  
-  
-
-  // this is used for for Alert function there
-  const [alert , setAlert]= useState('');  
- 
-  const showAlert =(message,type) =>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg :message,
-      type : type,
-    
+      msg: message,
+      type: type
+    });
 
-  })
-  // so this can enable the work on setAlert in 3 second 
-  setTimeout(() => {
-    setAlert(null);
-        
-  }, 1500);
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-
-}
-
-  const toggleMode =() =>{
-    if(mode=='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor ='gray';
-      // so th showAlert are used for print message 
-      showAlert("Dark Mode have been enabled","Success");
-    }
-    else{
+      document.body.style.backgroundColor = 'gray';
+      showAlert("Dark Mode has been enabled", "Success");
+    } else {
       setMode('light');
-      document.body.style.backgroundColor ='white';
-      // so th showAlert are used for print message 
-      showAlert("Light Mode have been enabled","Success");
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "Success");
     }
-
-  }
-  // this is used for gray and black ka last there
-
-  // const toggleMode1 =() =>{
-  //   if(mode=='light'){
-  //     setMode('dark');
-  //     document.body.style.backgroundColor ='blue';
-  //     // so th showAlert are used for print message 
-  //     showAlert("Dark Mode have been enabled","Success");
-  //   }
-  //   else{
-  //     setMode('light');
-  //     document.body.style.backgroundColor ='white';
-  //     // so th showAlert are used for print message 
-  //     showAlert("Light Mode have been enabled","Success");
-  //   }
-
-  // }
-  // same is method sy hum another button ko manage kaya gya 
-
-
+  };
 
   return (
-   <>
-
-  <Navbar aboutText="ABOUT US" mode = {mode}  toggleMode={toggleMode}    />
-  {/* // this is write in component folder and his file contain Navbar name (written in content)  and aslo this is used 
-  // of props there */}
-  <Alert   alert= {alert}/>
-  {/* // is used for Alert  */}
-  <div  className="container">
-  <TextForm   showAlert={showAlert} heading ="Enter the Text to Analysis" />
-  
-  </div>
-  {/* // is used for About */}
-  <About />
-  
-
-  
-   </>
+    <Router>
+      <Navbar aboutText="ABOUT US" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the Text to Analyze" />} />
+        </Routes>
+      </div>
+    </Router>
+    // Router used for  one click and move next page 
   );
 }
 
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
@@ -16,10 +17,11 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
+            <Link className="nav-link" to="/About">{props.aboutText}</Link>
+            {/* // is sy hum web page reload nhi hoo gya  */}
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
